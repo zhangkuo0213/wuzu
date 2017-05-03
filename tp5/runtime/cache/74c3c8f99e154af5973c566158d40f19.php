25ed3ce6e64eb20c3268b4d3178cc168a7468884
Your Name <707769680@qq.com>
2017-05-03 09:16:33 +0800

    update

--- tp5/runtime/cache/74c3c8f99e154af5973c566158d40f19.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:3:{i:0;a:3:{s:2:"id";i:126;s:10:"hotel_name";s:18:"北京崇阳酒店";s:10:"hotel_img1";s:5:"2.jpg";}i:1;a:3:{s:2:"id";i:130;s:10:"hotel_name";s:21:"北京新东方酒店";s:10:"hotel_img1";s:5:"2.jpg";}i:2;a:3:{s:2:"id";i:131;s:10:"hotel_name";s:18:"北京崇阳酒店";s:10:"hotel_img1";s:5:"2.jpg";}}
-?>

2bba8cb840788154fa996fcf2b1ffc73d324a124
niuben <2474246886@qq.com>
2017-05-03 11:30:55 +0800

    niu

--- tp5/runtime/cache/19e7b2fdac7dab7aa8c7f26afb26936d.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000000000a:3:{i:0;a:4:{s:6:"car_id";i:1;s:8:"car_name";s:12:"法拉利458";s:10:"car_salary";s:4:"8500";s:7:"car_img";s:38:"20160615144711537.jpg-1w363h227q85.jpg";}i:1;a:4:{s:6:"car_id";i:5;s:8:"car_name";s:9:"奥迪A8L";s:10:"car_salary";s:4:"1350";s:7:"car_img";s:37:"2015091014323992.jpg-1w363h227q85.jpg";}i:2;a:4:{s:6:"car_id";i:6;s:8:"car_name";s:9:"奔驰SLK";s:10:"car_salary";s:4:"1350";s:7:"car_img";s:38:"20160707094513718.jpg-1w363h227q85.jpg";}}
-?>
